9eeb93a462080f048de528515781d7123e852bbd
Li Sui <leesui0207@gmail.com>
2020-02-15 05:06:15 +0800

    added polybar, updated rofi

---
+++ manjaroI3/.config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch bar
+echo "---" | tee -a /tmp/polybar.log
+polybar mybar >>/tmp/polybar.log 2>&1 &
+
+
+echo "Bars launched..."
