4487b0e194fa2e22af268609e18fd250c48fb7ad
Li Sui <leesui0207@gmail.com>
2022-01-15 10:27:18 +1300

    updated powershell

--- win11/powershell/Microsoft.PowerShell_profile.ps1
+++ win11/powershell/Microsoft.PowerShell_profile.ps1
@@ -8,9 +8,11 @@ oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 
-
 # Utilities
 function which ($command) {
   Get-Command -Name $command -ErrorAction SilentlyContinue |
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
+#Alias
+Set-Alias subl 'F:\Softwares\Sublime Text 3\sublime_text.exe'
+Set-Alias grep findstr
